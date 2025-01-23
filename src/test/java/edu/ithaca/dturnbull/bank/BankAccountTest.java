5eda2755299ec89e4f75d4d2a86f6258ec6ab21f
1-Megan-1 <moconnor3@ithaca.edu>
2025-01-22 20:54:49 -0500

    Added more isEmailValid() method test cases, and formatted tests better.

--- src/test/java/edu/ithaca/dturnbull/bank/BankAccountTest.java
+++ src/test/java/edu/ithaca/dturnbull/bank/BankAccountTest.java
@@ -24,18 +24,25 @@ class BankAccountTest {
 
     @Test
     void isEmailValidTest(){
-        assertTrue(BankAccount.isEmailValid( "a@b.com"));   // valid email address
-        assertFalse( BankAccount.isEmailValid(""), "Emails must be invalid if they are blank");         // empty string
+        //Invalid Test Cases
+        assertFalse(BankAccount.isEmailValid(""), "Emails must be invalid if they are blank");         // empty string
         assertFalse(BankAccount.isEmailValid("@gmail.com"), "Emails must be invalid if they do not contain a username before the @ symbol");
         assertFalse(BankAccount.isEmailValid("em@il@gmail.com"), "Emails must be invalid if they contain more than one @ symbol");
         assertFalse(BankAccount.isEmailValid("hello"), "Emails must be invalid if they do not contain an @ or a domain and it's suffix");
         assertFalse(BankAccount.isEmailValid("*hello#%()@gmail.com"), "Emails must be invalid if containing illegal characters");
-        assertTrue(BankAccount.isEmailValid("elliot.oconnor@ithaca.edu"), "Emails must be valid if username contains periods (they should be ignored)");
         assertFalse(BankAccount.isEmailValid("elliot@ithaca"), "Emails must be invalid if there is no domain suffix (or period) present");
         assertFalse(BankAccount.isEmailValid("hello@.com"), "Emails must be invalid if there is no domain between the @ and suffix");
         assertFalse(BankAccount.isEmailValid("hello@gmail.superLongDomainSuffix"), "Emails must not be valid with domain suffixes longer than 3 characters");
-        assertTrue(BankAccount.isEmailValid("elliot@duck.io"), "Emails must be valid with 1-3 character long domain suffixes");
         assertFalse(BankAccount.isEmailValid("elliot@ithaca.i.o"), "Emails must only be valid if there is one period present after the @ symbol");
+        assertFalse(BankAccount.isEmailValid("el-@ithaca.edu"), "Emails must be invalid if the username ends in a special character");
+        assertFalse(BankAccount.isEmailValid("hello@ithac#.edu"), "Emails must be invalid if the domain contains an invalid special character");
+
+        //Valid Test Cases
+        assertTrue(BankAccount.isEmailValid("email@itha-ca.edu"), "Emails should be valid if domain contains a dash");
+        assertTrue(BankAccount.isEmailValid("user_name@gmail.com"), "Emails should be valid if the username contains an underscore.");
+        assertTrue(BankAccount.isEmailValid("elliot@duck.io"), "Emails must be valid with 1-3 character long domain suffixes");
+        assertTrue(BankAccount.isEmailValid("elliot.oconnor@ithaca.edu"), "Emails must be valid if username contains periods (they should be ignored)");
+        assertTrue(BankAccount.isEmailValid( "a@b.com"));   // valid email address
 
     }
 
